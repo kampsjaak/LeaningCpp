ccb0c68e2d0d8e6954222c6f66004361e056f8c7
kampsjaak <rotor3d@gmail.com>
2021-07-18 21:52:51 +0200

    matrix code part 2

---
+++ LeaningCpp/LeaningCpp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class MatrixCode {
+	public:
+		char _c = 12;
+		const char* trail = "abcdef";
+		unsigned int speed = 0;
+};
+
+unsigned int colums = 20;
+unsigned int rows = 50;
+bool bUpdate = true;
+
+int main()
+{
+	while (bUpdate)
+	{
+
+	}
+	return 0;
+}
+
+void printChar(int _c, int _r, char _char)
+{
+
+}
