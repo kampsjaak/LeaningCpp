bf62c9bee249d5f9b009d5bd9e22654872ab68b0
sjors <sjors@vertigogames.com>
2021-07-19 13:27:14 +0200

    string literals

--- LeaningCpp/LeaningCpp.cpp
+++ LeaningCpp/LeaningCpp.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
+#include <string.h>
 
 class MatrixCode {
+	private:
+		unsigned int column = 1;
+		const char* sLiteral = "immutable";
+		char trail[5];
+		unsigned int speed = 0; // 1, 2, 3
+		const char* GenerateString()
+		{
+			char newStr[] = "mutable";
+			//newStr[2] = 'c';
+			for (int i = 0; i < 5; i++)
+			{
+				newStr[i]= '1';
+			}
+
+			return newStr;
+		}
 	public:
-		char _c = 12;
-		const char* trail = "abcdef";
-		unsigned int speed = 0;
+		void Initilise(int _column, int _speed)
+		{
+			column = _column;
+			speed = _speed;
+		}
 };
 
 unsigned int colums = 20;
 unsigned int rows = 50;
 bool bUpdate = true;
 long unsigned int clock = 0;
+
 
 int main()
 {
