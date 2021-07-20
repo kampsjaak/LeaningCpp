4a0e8b23fb300df7faa447cdc0e75d94e5e0c9df
kampsjaak <sjors.janssen@rotorgames.nl>
2021-07-20 09:58:07 +0200

    view model controller

--- LeaningCpp/LeaningCpp.cpp
+++ LeaningCpp/LeaningCpp.cpp
@@ -5,58 +5,97 @@
 #include <windows.h > // input https://visualstudioclient.gallerycdn.vsassets.io/extensions/visualstudioclient/microsoftvisualstudio2017installerprojects/1.0.0/1620063166533/InstallerProjects.vsix
 #include <ctime> // random nummer generation
 
-class MatrixCode {
-	private:
-		short column = 1;
-		short row = 0;
-		const char* sLiteral = "immutable";
-		char trail[5];
-		unsigned int speed = 0; // 1, 2, 3
-		const char* GenerateString()
-		{
-			char newStr[] = "mutable";
-			//newStr[2] = 'c';
-			for (int i = 0; i < 5; i++)
-			{
-				newStr[i]= '1';
-			}
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD cursorPos = { 32, 12 };
 
-			return newStr;
-		}
-	public:
-		void Initilise(short _column, int _speed)
+class MatrixCodeView;
+class MatrixCodeModel;
+
+short screenColums = 20;
+short screenRows = 50;
+unsigned int programUpdateStep = 300; //ms
+const char programLinesMax = 20;
+
+class MatrixCodeView {
+private: 
+	MatrixCodeModel mcm;
+	std::string trail = "abcde";
+public:
+	std::string GenerateString()
+	{
+		std::string newStr = "mutable";
+		for (int i = 0; i < 5; i++)
 		{
-			column = _column;
-			speed = _speed;
+			newStr[i] = '1';
 		}
-		void Step()
+		return newStr;
+	}
+	void Draw()
+	{
+		for (char i = 0; i < 5; i++)
 		{
-			row = row + speed;
+			// add likely hood for 'decay' as i increases
+			PrintChar(mcm.column, mcm.row + i, &trail[i]);
 		}
+	}
 };
 
-short colums = 20;
-short rows = 50;
-bool bUpdate = true;
-unsigned int microseconds = 300;
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-COORD pos = { 32, 12 };
+class MatrixCodeModel {
+public:
+	short column = 1;
+	short row = 0;
+	unsigned char speed = 0; // 1, 2, 3
+	void Initilise(short _column, char _speed) // make proper constructor method
+	{
+		column = _column;
+		speed = _speed;
+	}
+	void Step()
+	{
+		row = row + speed;
+	}
+};
+
+MatrixCodeView programLines[programLinesMax];
+
+void PrintChar(short _x, short _y, char* _char)
+{
+	SetConsoleCursorPosition(hConsole, { _x, _y });
+	WriteConsole(hConsole, _char, 1, NULL, NULL);
+}
+
+char GetRandomChar()
+{
+	return rand() % 255;
+}
+
+void Initialise()
+{
+	for (char i = 0; i < programLinesMax; i++)
+	{
+		programLines[programLinesMax] = MatrixCodeModel();
+	}
+}
+
+void Draw()
+{
+	for (auto mcm : programLines)
+	{
+		mcm.Draw();
+	}
+}
 
 int main()
 {
+	Initialise();
 	srand(time(NULL)); // sets the seed for RNG
-	pos = { (short)(abs(rand()) % 50), (short)(abs(rand()) % 30) };
-	SetConsoleCursorPosition(hConsole, pos);
+	cursorPos = { (short)(abs(rand()) % 50), (short)(abs(rand()) % 30) };
+	SetConsoleCursorPosition(hConsole, cursorPos);
 
 	while (!GetAsyncKeyState(VK_ESCAPE))
 	{
 		WriteConsole(hConsole, "!", 1, NULL, NULL);
-		std::this_thread::sleep_for(std::chrono::milliseconds(microseconds));
+		std::this_thread::sleep_for(std::chrono::milliseconds(programUpdateStep));
 	}
 	return 0;
-}
-
-void printChar(short _x, short _y, char _char)
-{
-
 }
